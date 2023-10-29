dedfc41e5fa8cc6000c662cdf80d06e69d2a13b6
Attilius <leo20@freemail.hu>
2023-10-29 21:07:46 +0100

    Added Category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * Get the technologies for the category.
+     */
+    public function technologies(): HasMany
+    {
+        return $this->hasMany(Technology::class);
+    }
+}
