4e184752218705e4b4bd9e090d2a568e44b6c2ed
Attilius <leo20@freemail.hu>
2023-10-29 21:09:20 +0100

    Added Visitors.php

---
+++ app/Models/Visitors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visitors extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'type_of_browser',
+        'ip_address',
+        'date_of_visit',
+        'times'
+    ];
+}
