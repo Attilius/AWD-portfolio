36a1dfa69a1ce5e61f32116325701782a7daf07b
Attilius <leo20@freemail.hu>
2023-11-10 20:03:01 +0100

    Added ListSubtitle.vue

---
+++ resources/js/Components/ListSubtitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+const props = defineProps({
+    title: String
+});
+</script>
+
+<template>
+    <div class="flex my-5">
+        <h5 class="text-light_blue text-md italic capitalize">{{ title }}</h5>
+        <div class="ml-2">
+            <hr class="w-16 text-light_blue h-[1px] relative top-3" />
+        </div>
+    </div>
+</template>
