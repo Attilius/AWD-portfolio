39a25d0e3bdfee75179fdc1aaed722397409cdf8
Attilius <leo20@freemail.hu>
2023-11-10 20:06:53 +0100

    Modified app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,14 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleLeft, faDownload, faFilter, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faAvianex, faFacebookF, faTwitter, faLinkedinIn, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+library.add( faAngleLeft, faLinkedinIn, faGithub, faStackOverflow, faDownload, faFilter, faEye );
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +21,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
